8b75e0ce72be196d596e086e63b027253b92dc7c
0xcatrovacer <swarnabgarang@gmail.com>
2023-08-03 17:46:13 +0530

    [feat]: Added identity reconciliation for primary and secondary records

--- src/controllers/identityController.ts
+++ src/controllers/identityController.ts
@@ -7,50 +7,55 @@ import {
 interface ResultContact {
     primaryContactId: number;
     emails: string[];
-    phoneNumbers: number[];
+    phoneNumbers: string[];
     secondaryContactIds: number[];
 }
 
 export const identify = async (req: Request, res: Response) => {
-    const { phoneNumber, email } = req.body;
+    try {
+        const { phoneNumber, email } = req.body;
 
-    let relatedContacts: Contact[] =
-        await fetchAllContactsByPhoneNumberAndEmail(phoneNumber, email);
+        let relatedContacts: Contact[] =
+            await fetchAllContactsByPhoneNumberAndEmail(phoneNumber, email);
 
-    let emails: string[] = [];
-    let phoneNumbers: number[] = [];
-    let secondaryContactIds: number[] = [];
+        let emails: string[] = [];
+        let phoneNumbers: string[] = [];
+        let secondaryContactIds: number[] = [];
 
-    let contactResponse: ResultContact = {
-        primaryContactId: 0,
-        emails: [],
-        phoneNumbers: [],
-        secondaryContactIds: [],
-    };
+        let contactResponse: ResultContact = {
+            primaryContactId: 0,
+            emails: [],
+            phoneNumbers: [],
+            secondaryContactIds: [],
+        };
 
-    relatedContacts.map((contact: Contact) => {
-        if (contact.linkPrecedence === "primary") {
-            contactResponse["primaryContactId"] = contact.id;
+        relatedContacts.map((contact: Contact) => {
+            if (contact.linkPrecedence === "primary") {
+                contactResponse["primaryContactId"] = contact.id;
 
-            contact.email && emails.push(contact.email);
-            contact.phoneNumber && phoneNumbers.push(contact.phoneNumber);
-        } else if (contact.linkPrecedence === "secondary") {
-            !secondaryContactIds.includes(contact.id) &&
-                secondaryContactIds.push(contact.id);
+                contact.email && emails.push(contact.email);
+                contact.phoneNumber && phoneNumbers.push(contact.phoneNumber);
+            } else if (contact.linkPrecedence === "secondary") {
+                !secondaryContactIds.includes(contact.id) &&
+                    secondaryContactIds.push(contact.id);
 
-            contact.email &&
-                !emails.includes(contact.email) &&
-                emails.push(contact.email);
+                contact.email &&
+                    !emails.includes(contact.email) &&
+                    emails.push(contact.email);
 
-            contact.phoneNumber &&
-                !phoneNumbers.includes(contact.phoneNumber) &&
-                phoneNumbers.push(contact.phoneNumber);
-        }
-    });
+                contact.phoneNumber &&
+                    !phoneNumbers.includes(contact.phoneNumber) &&
+                    phoneNumbers.push(contact.phoneNumber);
+            }
+        });
 
-    contactResponse["emails"] = emails;
-    contactResponse["phoneNumbers"] = phoneNumbers;
-    contactResponse["secondaryContactIds"] = secondaryContactIds;
+        contactResponse["emails"] = emails;
+        contactResponse["phoneNumbers"] = phoneNumbers;
+        contactResponse["secondaryContactIds"] = secondaryContactIds;
 
-    return contactResponse;
+        res.status(200).send({ contact: contactResponse });
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Error");
+    }
 };
--- src/models/contact.ts
+++ src/models/contact.ts
@@ -3,7 +3,7 @@ import pool from "../database/dbConnection";
 
 export interface Contact {
     id: number;
-    phoneNumber: number;
+    phoneNumber: string;
     email: string;
     linkedId: number;
     linkPrecedence: "secondary" | "primary";
@@ -18,13 +18,51 @@ export const fetchAllContactsByPhoneNumberAndEmail = async (
 ): Promise<Contact[]> => {
     const client: PoolClient = await pool.connect();
 
-    const sqlQuery = `SELECT * FROM contact WHERE ${
-        phoneNumber && '"phoneNumber" = \'' + phoneNumber + "' OR"
-    } ${email && "email = '" + email + "'"}`;
+    let contacts: Contact[] = [];
 
-    const { rows } = await client.query(sqlQuery);
+    let sqlQuery: string;
+    if (!phoneNumber) {
+        sqlQuery = `SELECT * FROM public.contact WHERE email='${email}'`;
+    } else if (!email) {
+        sqlQuery = `SELECT * FROM public.contact WHERE "phoneNumber"='${phoneNumber}'`;
+    } else {
+        sqlQuery = `SELECT * FROM public.contact WHERE email='${email}' OR "phoneNumber"='${phoneNumber}'`;
+    }
+
+    const result = await client.query(sqlQuery);
+    const rows: Contact[] = result.rows;
+
+    contacts.push(...rows);
+
+    if (
+        contacts.some(
+            (contact: Contact) => contact.linkPrecedence === "primary"
+        )
+    ) {
+        let primaryContact = contacts.filter(
+            (contact) => contact.linkPrecedence === "primary"
+        );
+
+        let secondaryContactsSQLQuery = `SELECT * FROM public.contact WHERE "linkedId"='${primaryContact[0].id}'`;
+
+        const newResult = await client.query(secondaryContactsSQLQuery);
+        const newRows: Contact[] = newResult.rows;
+
+        contacts.push(...newRows);
+
+        client.release();
+
+        return contacts;
+    }
+
+    let primaryContactsSQLQuery = `SELECT * FROM public.contact WHERE id='${contacts[0].linkedId}'`;
+
+    const newResult = await client.query(primaryContactsSQLQuery);
+    const newRows: Contact[] = newResult.rows;
+
+    contacts.push(...newRows);
 
     client.release();
 
-    return rows;
+    return contacts;
 };
